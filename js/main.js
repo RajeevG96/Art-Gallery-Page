3b02c240ecca4d7a6341561b796a2a29f8f8f103
Rajeev Gill <gill96@live.co.uk>
2017-04-11 14:49:40 +0100

    Files Added

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#000',
+    lineColor: '#000'
+  });
+  var particles = document.getElementById('particles');
+  head.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
